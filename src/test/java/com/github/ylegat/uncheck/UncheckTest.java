fc3d17b2cc7261d59a61af7c582c1c19cf8d4c51
Nicolas MOREAU <nicolas-externe.moreau@erdf.fr>
2016-09-16 14:09:59 +0200

    Add an unchecked wrapper around a java.util.Function

--- src/test/java/com/github/ylegat/uncheck/UncheckTest.java
+++ src/test/java/com/github/ylegat/uncheck/UncheckTest.java
@@ -6,6 +6,9 @@ import static com.github.ylegat.uncheck.Uncheck.uncheck;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+
+import com.github.ylegat.uncheck.Uncheck.CheckedFunction;
 import org.junit.Test;
 import com.github.ylegat.uncheck.Uncheck.CheckedOperation;
 import com.github.ylegat.uncheck.Uncheck.CheckedSupplier;
@@ -31,6 +34,19 @@ public class UncheckTest {
 
         // Then
         assertThat(atomicBoolean.get()).isTrue();
+    }
+
+    @Test
+    public void should_transform_value() {
+        // Given
+        Integer value = 42;
+        Function<Integer, String> uncheckedTransform = uncheck(String::valueOf);
+
+        // When
+        String transformedValue = uncheckedTransform.apply(value);
+
+        // Then
+        assertThat(transformedValue).isEqualTo(String.valueOf(value));
     }
 
     @Test
@@ -80,4 +96,48 @@ public class UncheckTest {
                              .hasCause(exception);
     }
 
+    @Test
+    public void should_propagate_runtime_exception_from_transformation_application() {
+        // Given
+        RuntimeException runtimeException = new RuntimeException();
+        CheckedFunction<Integer, Object> transform = any -> {
+            throw runtimeException;
+        };
+
+        // When
+        Throwable throwable = catchThrowable(() -> uncheck(transform).apply(42));
+
+        // Then
+        assertThat(throwable).isSameAs(runtimeException);
+    }
+
+    @Test
+    public void should_create_unchecked_io_exception_from_transformation_application_io_exception() {
+        // Given
+        IOException ioException = new IOException();
+        CheckedFunction<Integer, Object> transform = any -> {
+            throw ioException;
+        };
+
+        // When
+        Throwable throwable = catchThrowable(() -> uncheck(transform).apply(42));
+
+        // Then
+        assertThat(throwable).isInstanceOf(UncheckedIOException.class).hasCause(ioException);
+    }
+
+    @Test
+    public void should_create_runtime_exception_from_transformation_application_checked_exception() {
+        // Given
+        Exception exception = new Exception();
+        CheckedFunction<Integer, Object> transform = any -> {
+            throw exception;
+        };
+
+        // When
+        Throwable throwable = catchThrowable(() -> uncheck(transform).apply(42));
+
+        // Then
+        assertThat(throwable).isInstanceOf(RuntimeException.class).hasCause(exception);
+    }
 }
